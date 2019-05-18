c60aa0d4f351ad36e13c857be1900b0bc7919246
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2019-05-18 11:30:52 -0700

    Clean up some one-letter vars.

--- rlbridge/bots/loaders.py
+++ rlbridge/bots/loaders.py
@@ -15,20 +15,20 @@ def load_bot_module(bot_type):
 
 
 def init_bot(bot_type, options, metadata):
-    m = load_bot_module(bot_type)
-    init_fn = getattr(m, 'init')
+    mod = load_bot_module(bot_type)
+    init_fn = getattr(mod, 'init')
     return init_fn(options, metadata)
 
 
 def save_bot(bot, outputfile):
     bot_type = bot.bot_type()
-    m = load_bot_module(bot_type)
-    save_fn = getattr(m, 'save')
+    mod = load_bot_module(bot_type)
+    save_fn = getattr(mod, 'save')
     with open_h5file_if_necessary(outputfile, 'w') as outf:
         outf.attrs['bot_type'] = bot_type
         metadata = outf.create_group('metadata')
-        for k, v in bot.metadata.items():
-            metadata.attrs[k] = v
+        for key, value in bot.metadata.items():
+            metadata.attrs[key] = value
         bot_data = outf.create_group('bot_data')
         save_fn(bot, bot_data)
 
@@ -36,11 +36,11 @@ def save_bot(bot, outputfile):
 def load_bot(inputfile):
     with open_h5file_if_necessary(inputfile, 'r') as inf:
         bot_type = inf.attrs['bot_type']
-        m = load_bot_module(bot_type)
-        load_fn = getattr(m, 'load')
+        mod = load_bot_module(bot_type)
+        load_fn = getattr(mod, 'load')
         metadata_group = inf['metadata']
         metadata = {}
-        for k in metadata_group.attrs:
-            metadata[k] = metadata_group.attrs[k]
+        for key in metadata_group.attrs:
+            metadata[key] = metadata_group.attrs[key]
         bot_data = inf['bot_data']
         return load_fn(bot_data, metadata)
